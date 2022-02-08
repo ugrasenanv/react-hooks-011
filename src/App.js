f8ebf1cd350fff54f951f7e97c8e64ecef26b21f
ugrasenanv <ugrasenanv@gmail.com>
2022-02-09 00:00:27 +0530

    2 files modified, 3 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import React from "react";
-import "./style.css";
 
-export default function App() {
+import React, { useState } from 'react';
+import ResourceList from './components/ResourceList';
+import UserList from './components/UserList';
+
+const App = () => {
+  // Array destruction
+  const [resource, setResource] = useState('posts');
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <div>
+        <button onClick={() => setResource('posts')}>Posts</button>
+        <button onClick={() => setResource('todos')} >Todos</button>
+        <button onClick={() => setResource('users')} >Users</button>
+      </div>
+      <UserList ></UserList>
+      <ResourceList resource={resource} ></ResourceList>
     </div>
   );
 }
+
+export default App;
+
+// import React from "react";
+// import "./style.css";
+
+// export default function App() {
+//   return (
+//     <div>
+//       <h1>Hello StackBlitz!</h1>
+//       <p>Start editing to see some magic happen :)</p>
+//     </div>
+//   );
+// }
