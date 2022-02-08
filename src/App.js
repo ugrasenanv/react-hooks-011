33f463479594cebbc0e3e3e5e0bbd37a214b1f96
ugrasenanv <ugrasenanv@gmail.com>
2022-02-09 00:51:02 +0530

    3 files modified

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React, { useState } from 'react';
 import ResourceList from './components/ResourceList';
 import UserList from './components/UserList';
-
+import './style.css';
 const App = () => {
   // Array destruction
   const [resource, setResource] = useState('posts');
 
   return (
     <div>
-      <div>
+      <div class="btn-wrapper">
         <button onClick={() => setResource('posts')}>Posts</button>
         <button onClick={() => setResource('todos')}>Todos</button>
         <button onClick={() => setResource('users')}>Users</button>
       </div>
-      <UserList></UserList>
+      {/* <UserList></UserList> */}
       <ResourceList resource={resource}></ResourceList>
     </div>
   );
--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -6,7 +6,7 @@ const ResourceList = ({ resource }) => {
   const resources = useResources(resource)
 
   return (
-    <ul>{resources.map(record => <li key={record.id}>{record.title}</li>)}</ul>
+    <ul>{resources.map(record => <li key={record.id}>{record.title} {record.name}</li>)}</ul>
   );
 }
 
